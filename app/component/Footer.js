1350d7b206e306002709135ae88b3d17b6fa7dba
Matt <mattkeegan@btinternet.com>
2018-02-27 13:40:27 +0000

    Update link to Github

--- app/component/Footer.js
+++ app/component/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return(
         <div className="row justify-content-center">
             <div className="col-12 col-md-11 col-lg-10 footer">
-                <h5 className='text-center' id='made-by'>Made by <a href='https://codepen.io/mattkeegan20/' target='_blank'>Matt K</a></h5>
+                <h5 className='text-center' id='made-by'>Made by <a href='https://github.com/yobananaboy' target='_blank'>Matt K</a></h5>
             </div>
         </div>
       );
